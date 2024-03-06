a1eddc1a2ab449df164c2fac36cf362a53c8068c
CJ3016994 <CJ3016994@LAB01-PC41>
2024-03-06 10:45:26 -0300

    Adicionar arquivos de projeto.

---
+++ ATV2_CJ3016994/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATV2_CJ3016994
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            //Console.Write("Digite um valor em metros: ");
+            //double metros = double.Parse(Console.ReadLine());
+
+            //double centimetros = metros * 100;
+            //double milimetros = metros * 1000;
+
+            //Console.WriteLine($"{metros} metros equivalem a {centimetros} centímetros e {milimetros} milímetros.");
+            //Console.ReadKey();
+        }
+        //2a
+        //Console.Write("Digite a temperatura em Fahrenheit: ");
+        //float fahrenheit = float.Parse(Console.ReadLine());
+
+        //float celsius = (fahrenheit - 32.0f) * (5.0f / 9.0f);
+
+        //Console.WriteLine($"{fahrenheit} graus Fahrenheit correspondem a {celsius} graus Celsius.");
+
+            }
+    //2b
+    //Console.Write("Digite a temperatura em Fahrenheit: ");
+       // int fahrenheit = int.Parse(Console.ReadLine());
+
+    //int celsius = (fahrenheit - 32) * (5 / 9);
+
+   // Console.WriteLine($"{fahrenheit} graus Fahrenheit correspondem a {celsius} graus Celsius.");
+   //Console.Readkey();
+    }
+//3
+//Console.Write("Digite seu peso em kg: ");
+//double peso = double.Parse(Console.ReadLine());
+
+//Console.Write("Digite sua altura em metros: ");
+//double altura = double.Parse(Console.ReadLine());
+
+//double imc = CalcularIMC(peso, altura);
+
+//Console.WriteLine($"Seu IMC é: {imc:F2}");
+//    }
+
+//    static double CalcularIMC(double peso, double altura)
+//{
+//    return peso / (altura * altura);
+
+//3
+//Console.Write("Digite seu peso em kg: ");
+//double peso = double.Parse(Console.ReadLine());
+
+//Console.Write("Digite sua altura em metros: ");
+//double altura = double.Parse(Console.ReadLine());
+
+//double imc = CalcularIMC(peso, altura);
+
+//Console.WriteLine($"Seu IMC é: {imc:F2}");
+//    }
+
+//    static double CalcularIMC(double peso, double altura)
+//{
+//    return peso / (altura * altura);
+//    Console.ReadKey();
+//}
+
+
+
+
